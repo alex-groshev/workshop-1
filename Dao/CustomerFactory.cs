53192c0d2f338d12925c3e09a6500893fc1cd381
Alexey Groshev <aleksejs.grosevs@gmail.com>
2016-05-24 23:24:37 +0100

    Added CustomerFactory which returns/builds customers. Most likely it will evolve into a builder (or set of queries).

---
+++ Dao/CustomerFactory.cs
@@ -0,0 +1,54 @@
+﻿using Domain;
+
+namespace Dao
+{
+    public class CustomerFactory
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPaymentRepository _paymentRepository;
+
+        public CustomerFactory(IUserRepository userRepository,
+            IPaymentRepository paymentRepository)
+        {
+            _userRepository = userRepository;
+            _paymentRepository = paymentRepository;
+        }
+
+        public BaseCustomer Create(int id)
+        {
+            BaseCustomer result;
+
+            var user = _userRepository.FindById(id);
+            if (user == null)
+            {
+                // return null for the time being.
+                // it would be nice to have NullCustomer so it could be processed safely.
+                return null;
+            }
+
+            switch (user.UserType)
+            {
+                case UserType.Customer:
+                    result = new Customer(user.Id, user.Name, user.UserStatus,
+                        _paymentRepository.GetUserPayments(user.Id));
+                    break;
+                case UserType.SilverCustomer:
+                    result = new SilverCustomer(user.Id, user.Name, user.UserStatus,
+                        _paymentRepository.GetUserPayments(user.Id));
+                    break;
+                case UserType.GoldCustomer:
+                    result = new GoldCustomer(user.Id, user.Name, user.UserStatus,
+                        _paymentRepository.GetUserPayments(user.Id));
+                    break;
+                default:
+                    //case UserType.Admin:
+                    //case UserType.Support:
+                    // we don't need technical users, return null for the time being.
+                    // it would be nice to have NullCustomer so it could be processed safely.
+                    return null;
+            }
+
+            return result;
+        }
+    }
+}
